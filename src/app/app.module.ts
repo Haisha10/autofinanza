c62c65f12af3b11f0a2d5d8c5602836a47bd4e30
AlvaroL <u202012770@upc.edu.pe>
2023-11-01 13:55:58 -0500

    add: components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoanComponent } from './components/loan/loan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    LoanComponent
   ],
   imports: [
     BrowserModule,
