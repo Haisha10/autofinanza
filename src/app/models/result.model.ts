2f27a55ae5982dce61a0c5b2a4be9581db60f51f
AlvaroL <u202012770@upc.edu.pe>
2023-11-20 22:20:07 -0500

    Auto-generated commit [ci skip]

--- src/app/models/result.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Result {
-  n: number;
-  gracia: string;
-  saldo_inicial: number;
-  interes: number;
-  amortizacion: number;
-  cuota: number;
-  saldo_final: number;
-}
--- src/app/user-info.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./models/user.model";
-
-export var currentUser: User = {
-  id: 0,
-  firstName: "",
-  lastName: "",
-  login: "",
-  token: ""
-};
